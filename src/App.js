064e0ab6255123c333f20dd92c1494c812129a59
Being-Murshid <murshid71704@gmail.com>
2023-01-25 13:36:27 +0500

    this is my first project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,47 @@
 import Navbar from './components/Navbar'
 import Form  from './components/Form';
 import About from './components/About';
+import { useState } from 'react';
+import Alert from './components/Alert';
 function App() {
+  const [mode , setmode]= useState('light')
+  const [label, setlabel]= useState("Enable Light Mode")
+  const [alert, setalert]= useState (null)
+
+  const showalert=(message, type)=>{
+    setalert({
+      msg: message,
+      type : type
+    })
+    setTimeout(() => {
+      setalert(null)
+    }, 3000);
+  }
+  const toggle =()=>{
+    if(mode === "light"){
+      setmode("dark")
+      setlabel('Enable Light Mode')
+      document.body.style.backgroundColor="gray"
+      showalert("Dark Mode has Been Enabled", "success")
+      document.title="Textutils -  Dark Mode"
+
+    }
+    else{
+      setmode("light")
+      setlabel("Enable Dark Mode")
+      document.body.style.backgroundColor="white"
+      showalert("Light Mode has Been Enabled", "success")
+      document.title="Textutils - Light Mode"
+
+    }
+  }
   return (
      <>
-     <Navbar title="TextUtils"  abouttitle="About us"/>
+     <Navbar title="TextUtils" label={label}  abouttitle="About us" mode={mode} toggle={toggle}/>
+     <Alert alert={alert}/>
      <div className='container my-3'>
-
-     {/* <Form heading="Enter the Text to Analyze"/> */}
-     <About/>
+     <Form heading="Enter the Text to Analyze" showalert={showalert} mode={mode}/>
+     {/* <About/> */}
      </div>
      </>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,16 +5,21 @@ export default function Form(props) {
         // console.log('convert to upper case ' + Text)
         let newText = Text.toUpperCase()
         setText(newText)
+        props.showalert("Converted to Uppercase", "succees")
     }
     const lowercasebtn = () => {
         // console.log('convert to upper case ' + Text)
         let newText = Text.toLowerCase()
         setText(newText)
+        props.showalert("Converted to Lowercase", "succees")
+
     }
     const clearbtn = () => {
         // console.log('convert to upper case ' + Text)
         let newText = '';
-        setText(newText)    
+        setText(newText) 
+        props.showalert("Clear All text from the Box", "danger")
+
     }
     const handleonchange = (event) => {
         // console.log('handle on the changee ')
@@ -24,21 +29,21 @@ export default function Form(props) {
 
     return (
         <>
-            <div className="container">
+            <div className="container" style={{color: props.mode === "dark"?"white":"black" }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" onChange={handleonchange} id="mybox" rows="8" value={Text} />
+                    <textarea className="form-control" style={{backgroundColor: props.mode ==="dark"? "gray": "white", color:props.mode ==="dark"? "white" : "dark"}} onChange={handleonchange} id="mybox" rows="8" value={Text} />
                     <button className="btn btn-primary my-3 mx-1" onClick={uppercasebtn}>convert to uppercase</button>
                     <button className="btn btn-primary my-3 mx-1 " onClick={lowercasebtn}>convert to lowercase</button>
                     <button className="btn btn-primary my-3 mx-1 " onClick={clearbtn}>Clear Text</button>
-                </div>
+                </div>   
             </div>
-            <div className="container my-2">
+            <div className="container my-2" style={{color: props.mode === "dark"?"white":"black" }}>
                 <h1>Your Text Summary </h1>
                 <p>{Text.split(" ").length} words and {Text.length} character</p>
                 <p>{0.008 * Text.split(" ").length } Minutes   to read</p>
                 <h2>Preview</h2>
-                <p>{Text}</p>
+                <p>{Text.length>0?Text:"Enter something to preview here"}</p>
             </div>
         </>
     )
